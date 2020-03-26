9104fa8ce394e7fb04612c70e1bd8bd5ddf1d570
imane <benbenimane@hotmail.com>
2020-03-26 14:40:31 +0100

    useEffect  and  start button, using interval, adding trim  to  format  00:00

--- src/components/timeLeft.jsx
+++ src/components/timeLeft.jsx
@@ -1,15 +1,37 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import moment from "moment";
 import momentDurationFormatSetup from 'moment-duration-format'
 
-momentDurationFormatSetup(moment)
+momentDurationFormatSetup(moment);
 
 const TimeLeft = ({sessionlength}) =>{
     const [timeLeft, setTimeLeft]  = useState(sessionlength);
-
     const  formattedTimeLeft = moment.duration(timeLeft,'s').format('mm:ss');
 
-    return  <div>{formattedTimeLeft} </div>
+    //change timeleft whenever sessionlength changes, useEffect will listen to the state of session length
+    useEffect(
+        () => {
+        setTimeLeft(sessionlength)
+        }, [sessionlength]
+    );
+
+    //prevtime is a built in method that we can  use for  the setter
+    const start = () =>{
+        setInterval(() =>{
+        setTimeLeft(prevTimeLeft => prevTimeLeft -1)
+        },1000);
+    };
+
+    const end  =  ()  => {
+
+
+    };
+
+    return  <div>
+    <div>{formattedTimeLeft}</div>
+    <button onClick={start}>start</button>
+    <button>end</button>
+    </div>
 
 };
 
