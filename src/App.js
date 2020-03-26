a4802953746d9074989a0affc17f89ff9645804c
imane <benbenimane@hotmail.com>
2020-03-26 13:28:26 +0100

    uplifiting state for  session and break, using props  to pass  variables  to session

--- src/App.js
+++ src/App.js
@@ -4,12 +4,29 @@ import './App.css';
 import Session from "./components/session";
 
 function App() {
+  //5 minutes is 300 seconds
+  const [sessionlength, setsessionlength] = useState(300);
 
+  const decrement =() => {
+    const newsessionLength = sessionlength -60;
+    if(newsessionLength <0){
+      setsessionlength(0)
+    }else{
+      setsessionlength(newsessionLength);
+    }
+  };
+
+  const increment =()=>{
+    setsessionlength(sessionlength+60)
+  };
 
   return (
     <div className="App">
     <Break/>
-    <Session/>
+    <Session  sessionlength={sessionlength}
+    decrement ={decrement}
+    increment = {increment}
+    />
     </div>
   );
 }
--- src/components/session.jsx
+++ src/components/session.jsx
@@ -1,31 +1,17 @@
 import React, {useState} from "react";
 import moment from "moment";
 
-const Session = () => {
-    //5 minutes is 300 seconds
-    const [sessionlength, setsessionlength] = useState(300);
+const Session = props => {
 
-    const decrement =() => {
-        const newsessionLength = sessionlength -60;
-        if(newsessionLength <0){
-            setsessionlength(0)
-        }else{
-            setsessionlength(newsessionLength);
-        }
-    };
-
-    const increment =()=>{
-        setsessionlength(sessionlength+60)
-    };
     //using moment library, we can convert seconds  to minutes
-    const sessionLengthInMinutes = moment.duration(sessionlength, 's').minutes()
+    const sessionLengthInMinutes = moment.duration(props.sessionlength, 's').minutes()
 
     return(
         <div>
             <p id='break-label'>break</p>
             <p id="break-length">{sessionLengthInMinutes}</p>
-            <button id="break-decrement" onClick={decrement}>-</button>
-            <button id="break-increment" onClick={increment}>+</button>
+            <button id="break-decrement" onClick={props.decrement}>-</button>
+            <button id="break-increment" onClick={props.increment}>+</button>
         </div>
     )
 };
