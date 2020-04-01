94bed6aa4c488958ae4bf059e01091645079882f
imane <benbenimane@hotmail.com>
2020-04-01 16:26:20 +0200

    Updates

---
+++ precache-manifest.05faff97a159a99941f2a5078725eb3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0e2826a15260ecbb0e2d1aa203186ee",
+    "url": "/pomodoro2/index.html"
+  },
+  {
+    "revision": "34e11bdac78b946e713e",
+    "url": "/pomodoro2/static/css/main.1dded214.chunk.css"
+  },
+  {
+    "revision": "a6ed1eac03e32d3d4f77",
+    "url": "/pomodoro2/static/js/2.bae497da.chunk.js"
+  },
+  {
+    "revision": "5d7719635e368d1c17de69a19973b100",
+    "url": "/pomodoro2/static/js/2.bae497da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34e11bdac78b946e713e",
+    "url": "/pomodoro2/static/js/main.dfa99d27.chunk.js"
+  },
+  {
+    "revision": "0a6e8e01103fec678c95",
+    "url": "/pomodoro2/static/js/runtime-main.1995af73.js"
+  }
+]);
